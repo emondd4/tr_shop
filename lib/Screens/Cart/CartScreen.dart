cbcc26484ed1618ed0a025a270383032b371cd74
Emon Hossain <emondd4@gmail.com>
2024-02-17 01:32:08 +0600

    till increment decrement button done

--- lib/Screens/Cart/CartScreen.dart
+++ lib/Screens/Cart/CartScreen.dart
@@ -59,135 +59,129 @@ class _CartScreenState extends State<CartScreen> {
   }
 
   Widget productItem(int index){
-
-    return InkWell(
-      onTap: () {
-        //controller.gotoDetailsPage(controller.productListResponse.value!.data![index]);
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.0),color: Colors.white),
-          child: Row(
-            children: [
-              Container(
-                height: 123,
-                width: 10,
-                decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(15.0),
-                        bottomLeft: Radius.circular(15.0)),
-                    gradient: LinearGradient(colors: [AppColors.instance.appPrimacyColor, AppColors.instance.appSecondaryColor])),
-              ),
-              const SizedBox(
-                width: 10.0,
-              ),
-              Expanded(
-                  child: SizedBox(
-                    height: 123,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          controller.cartList.value?[index].name ?? "Loading...",
-                          style: GoogleFonts.nunito(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w700,
-                              color: AppColors.instance.appSecondaryColor),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        Text(
-                          controller.cartList.value?[index].des ?? "Loading...",
-                          style: GoogleFonts.nunito(
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w700,
-                              color: AppColors.instance.appSecondaryColor),
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                    ),
-                  )),
-              const SizedBox(
-                width: 10.0,
-              ),
-              Column(
-                children: [
-                  Text("\$${controller.cartList.value?[index].price}",
-                    style: GoogleFonts.nunito(
-                        fontSize: 22.0,
-                        fontWeight: FontWeight.w600,
-                        color: AppColors.instance.appPrimacyColor),
-                  ),
-                  const SizedBox(height: 20,),
-                  Row(
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.0),color: Colors.white),
+        child: Row(
+          children: [
+            Container(
+              height: 123,
+              width: 10,
+              decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15.0),
+                      bottomLeft: Radius.circular(15.0)),
+                  gradient: LinearGradient(colors: [AppColors.instance.appPrimacyColor, AppColors.instance.appSecondaryColor])),
+            ),
+            const SizedBox(
+              width: 10.0,
+            ),
+            Expanded(
+                child: SizedBox(
+                  height: 123,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      InkWell(
-                        onTap: () {
-
-                        },
-                        child: Container(
-                          height: 36.0,
-                          width: 36.0,
-                          decoration: BoxDecoration(
-                            color: AppColors.instance.appPrimacyColor,
-                            borderRadius: BorderRadius.circular(8.0)
-                          ),
-                          child: Center(
-                            child: Text("-",
-                              style: GoogleFonts.nunito(
-                                  fontSize: 22.0,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.white),
-                            ),
-                          ),
-                        ),
+                      Text(
+                        controller.cartList.value?[index].name ?? "Loading...",
+                        style: GoogleFonts.nunito(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w700,
+                            color: AppColors.instance.appSecondaryColor),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      Container(
+                      Text(
+                        controller.cartList.value?[index].des ?? "Loading...",
+                        style: GoogleFonts.nunito(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.w700,
+                            color: AppColors.instance.appSecondaryColor),
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                )),
+            const SizedBox(
+              width: 10.0,
+            ),
+            Column(
+              children: [
+                Text("\$${controller.cartList.value?[index].price}",
+                  style: GoogleFonts.nunito(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.instance.appPrimacyColor),
+                ),
+                const SizedBox(height: 20,),
+                Row(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        controller.decreaseItem(index);
+                      },
+                      child: Container(
                         height: 36.0,
                         width: 36.0,
                         decoration: BoxDecoration(
-                          color: Colors.white,
+                          color: AppColors.instance.appPrimacyColor,
                           borderRadius: BorderRadius.circular(8.0)
                         ),
                         child: Center(
-                          child: Text(controller.box.getAt(index)?.price.toString() ?? "1",
+                          child: Text("-",
                             style: GoogleFonts.nunito(
-                                fontSize: 25.0,
+                                fontSize: 22.0,
                                 fontWeight: FontWeight.w600,
-                                color: AppColors.instance.appPrimacyColor),
+                                color: Colors.white),
                           ),
                         ),
                       ),
-                      InkWell(
-                        onTap: () {
-
-                        },
-                        child: Container(
-                          height: 36.0,
-                          width: 36.0,
-                          decoration: BoxDecoration(
-                            color: AppColors.instance.appPrimacyColor,
-                            borderRadius: BorderRadius.circular(8.0)
-                          ),
-                          child: Center(
-                            child: Text("+",
-                              style: GoogleFonts.nunito(
-                                  fontSize: 22.0,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.white),
-                            ),
+                    ),
+                    Container(
+                      height: 36.0,
+                      width: 36.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.0)
+                      ),
+                      child: Center(
+                        child: Text(controller.box.getAt(index)?.price.toString() ?? "1",
+                          style: GoogleFonts.nunito(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.w600,
+                              color: AppColors.instance.appPrimacyColor),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        controller.increaseItem(index);
+                      },
+                      child: Container(
+                        height: 36.0,
+                        width: 36.0,
+                        decoration: BoxDecoration(
+                          color: AppColors.instance.appPrimacyColor,
+                          borderRadius: BorderRadius.circular(8.0)
+                        ),
+                        child: Center(
+                          child: Text("+",
+                            style: GoogleFonts.nunito(
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white),
                           ),
                         ),
                       ),
-                    ],
-                  )
-                ],
-              )
-            ],
-          ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
